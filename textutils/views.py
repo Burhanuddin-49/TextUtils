3be22d920375c41e57e15f1c6f8bcd1ed9892914
Burhanuddin-49 <75224524+Burhanuddin-49@users.noreply.github.com>
2021-01-12 19:23:48 +0300

    Update views.py

--- textutils/views.py
+++ textutils/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index1.html')
+    return render(request, 'index.html')
     # return HttpResponse("Home")
 
 
